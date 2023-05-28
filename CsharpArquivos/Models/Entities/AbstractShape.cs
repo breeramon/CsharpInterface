0ce7bf2a9cb79c3fb1fed9dc1b15184994a7452c
Breno Ramon <breeramon@gmail.com>
2023-05-28 12:52:20 -0300

    Herança com Interfaces

---
+++ CsharpArquivos/Models/Entities/AbstractShape.cs
@@ -0,0 +1,16 @@
+﻿using CsharpInterface.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpInterface.Models.Entities
+{
+    abstract class AbstractShape : IShape
+    {
+        public Color Color { get; set; }
+
+        public abstract double Area();
+    }
+}
---
+++ CsharpArquivos/Program.cs
@@ -0,0 +1,14 @@
+﻿using CsharpInterface.Models.Entities;
+using CsharpInterface.Models.Enums;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        IShape s1 = new Circle() { Radius = 2, Color = Color.White };
+        IShape s2 = new Rectangle() { Width = 3.5, Height = 4.2, Color = Color.Black };
+
+        Console.WriteLine(s1);
+        Console.WriteLine(s2);
+    }
+}
